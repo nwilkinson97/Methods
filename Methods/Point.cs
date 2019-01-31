705ecec1b6672401cfebcff58ed17b7d113613a9
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 10:01:59 -0500

    Commented Code

--- Methods/Point.cs
+++ Methods/Point.cs
@@ -4,9 +4,11 @@ namespace Methods
 {
     public class Point
     {
+        // X & Y Fields for Coordinates
         public int X;
         public int Y;
 
+        //  Class Constructor
         public Point(int x, int y)
         {
             this.X = x;
@@ -15,15 +17,17 @@ namespace Methods
 
         public void Move(int x, int y)
         {
+            // Pass a New Point to the Move Method
             Move(new Point(x, y));
         }
 
         public void Move(Point newLocation)
         {
-
+            // Throw Exception if passed a NULL value (DEFENSIVE PROGRAMMING)
             if (newLocation == null)
                 throw new ArgumentNullException("newLocation");
-
+            
+            // Set the Location of the pointer
             this.X = newLocation.X;
             this.Y = newLocation.Y;
         }
