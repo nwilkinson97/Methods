34dfeb35d65ad5e7a39f997798679f3e013bf176
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 10:05:42 -0500

    Try Catch Block
    Here I used the keyboard shortcut by highlighting all the code in the main menu and pressed {CTRL} + K + S.  This opened a new window to surround this code with a code snippet.  I typed "try" and pressed {TAB}.  This surround the original code in a try catch block.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,21 +4,29 @@
     {
         static void Main(string[] args)
         {
-            // Create an Instance of the Point Class
-            var point = new Point(10, 20);
+            try
+            {
+                // Create an Instance of the Point Class
+                var point = new Point(10, 20);
 
-            //  Pass the X & Y Coordinates as Integers and Display the Location
-            point.Move(100, 200);
-            System.Console.WriteLine("Point is at x {0} y {1}", point.X, point.Y);
+                //  Pass the X & Y Coordinates as Integers and Display the Location
+                point.Move(100, 200);
+                System.Console.WriteLine("Point is at x {0} y {1}", point.X, point.Y);
 
-            //  Pass the X & Y Coordinates as a new Point and Display the Location
-            point.Move(new Point(40, 60));
-            System.Console.WriteLine("Point is at x {0} y {1}", point.X, point.Y);
+                //  Pass the X & Y Coordinates as a new Point and Display the Location
+                point.Move(new Point(40, 60));
+                System.Console.WriteLine("Point is at x {0} y {1}", point.X, point.Y);
 
-            // Lets now Change things up and pass a null value to the Move Method
-            // Note:  When we run the code an error is thrown.  Because we have not
-            //        done anything to handle the error.  
-            point.Move(null);
+                // Lets now Change things up and pass a null value to the Move Method
+                // Note:  When we run the code an error is thrown.  Because we have not
+                //        done anything to handle the error.  
+                point.Move(null);
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
