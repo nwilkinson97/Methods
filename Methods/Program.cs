873dbb6b49cb70a9a6411929cb465582525d0f53
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 07:53:51 -0500

    Created a Point Class
    This Class is used to store the location of a pointer on the screen.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,5 +1,16 @@
 ﻿namespace Methods
 {
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
