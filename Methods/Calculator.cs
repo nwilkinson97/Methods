3e7b5ab4929d09c02ee3919c9ac7bf954f5f899c
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 10:14:05 -0500

    Create New Class Calculator
    In this step I created and moved the class to a new file.  using the quick actions shortcut.

---
+++ Methods/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Methods
+{
+    public class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
