a3127f5b4b36e18a8164ea2620c7ba81dd22c4a3
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 07:58:00 -0500

    Moved Class to New FIle
    Moved class using Keyboard Shortcut for Quick Actions {CTRL} + .    Additionally if you need to open the file you may use the keyboard shortcut to access the Solution Explorer {CTRL} + L then use {UP} or {DOWN} arrows to select a file and Finally use the {F7} key to open the file.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,16 +1,5 @@
 ﻿namespace Methods
 {
-    public class Point
-    {
-        public int X;
-        public int Y;
-
-        public Point(int x, int y)
-        {
-            this.X = x;
-            this.Y = y;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
