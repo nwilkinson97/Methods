ca65a8b6b8c7f9a831732bcb750d8c7e3ee4a36b
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 09:45:27 -0500

    Added Comments
    Comments Added for readability.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,12 +4,14 @@
     {
         static void Main(string[] args)
         {
+            // Create an Instance of the Point Class
             var point = new Point(10, 20);
 
+            //  Pass the X & Y Coordinates as Integers and Display the Location
             point.Move(100, 200);
             System.Console.WriteLine("Point is at x {0} y {1}", point.X, point.Y);
 
-
+            //  Pass the X & Y Coordinates as a new Point and Display the Location
             point.Move(new Point(40, 60));
             System.Console.WriteLine("Point is at x {0} y {1}", point.X, point.Y);
         }
