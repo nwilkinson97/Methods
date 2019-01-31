c33ec0c9b155f4545276fc8ec7689b8ed6e170f0
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-31 09:43:20 -0500

    Using The Move Method
    Here we use the Move Method and implement both implementations (Override) of the Move Class.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            var point = new Point(10, 20);
+
+            point.Move(100, 200);
+            System.Console.WriteLine("Point is at x {0} y {1}", point.X, point.Y);
+
+
+            point.Move(new Point(40, 60));
+            System.Console.WriteLine("Point is at x {0} y {1}", point.X, point.Y);
         }
     }
 }
